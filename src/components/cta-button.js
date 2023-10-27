36bca7b8b6930f7686020fe1cdc61ea4f0382af5
suejinkim20 <kim.suejin.20@gmail.com>
2023-10-27 00:03:29 -0400

    abstract out cta button into its own component

---
+++ src/components/cta-button.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+  AspectRatio, Card, CardOverflow,
+  Link as JoyLink, Sheet,
+} from '@mui/joy'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { Link } from './link'
+
+export const CtaButton = ({
+  background_image, href, title
+}) => {
+  const bgImage = getImage(background_image)
+  return (
+    <Card sx={{
+      overflow: 'hidden',
+      position: 'relative',
+      '.card-content': {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: '65px',
+        backgroundColor: 'azure',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+    }}>
+      <CardOverflow>
+        <AspectRatio>
+          <GatsbyImage image={ bgImage } alt="" />
+        </AspectRatio>
+      </CardOverflow>
+      <Sheet className="card-content">
+        <JoyLink
+          overlay
+          component={ Link } to={ href }
+          className="card-title"
+        >{ title }</JoyLink>
+        {console.log(background_image)}
+      </Sheet>
+    </Card>
+  )
+}

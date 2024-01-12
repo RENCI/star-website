355cdccb361f30708403ef1bda27e51b7ab0783d
suejinkim20 <kim.suejin.20@gmail.com>
2024-01-12 14:20:20 -0500

    update footer to have both star and renci logos

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ import { Sheet, Typography, Box, Container, Grid, useTheme } from '@mui/joy'
 // import { Container } from './container'
 import { Link } from './link'
 import renciLogo from '../images/renci.png'
+import starLogo from '../images/star-logo-color.png'
 
 export const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -21,10 +22,10 @@ export const Footer = () => {
     <Box
       sx={{
         background: '#ddd',
-        padding: '1rem 0',
+        padding: '2.5rem 0',
         color: theme.palette.text.primary,
         '& .link-group': {
-          paddingTop: '1rem',
+          // paddingTop: '1rem',
         },
         '& .link-list': {
           padding: 0,
@@ -37,28 +38,37 @@ export const Footer = () => {
     >
       <Container maxWidth="lg" >
         <Grid container>
-          <Grid item xs={ 12 } md={ 8 } lg={ 8 }>
-            <Box>
+          <Grid item xs={ 12 } sm={ 8 } md={ 9 } lg={ 9 }>
+
               <Box component="span" sx={{
                 display: 'block',
-                minHeight: '60px',
-                minWidth: '120px',
-                backgroundImage: `url(${ renciLogo })`,
+                minHeight: '50px',
+                minWidth: '110px',
+                backgroundImage: `url(${ starLogo })`,
                 backgroundSize: 'contain',
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: '0% 50%',
               }} />
               <p>
-                RENCI <br />
+                Student Achievement at RENCI<br />
                 Europa Center <br />
                 100 Europa Drive, Suite 540 <br />
                 Chapel Hill, NC  27517
               </p>
-            </Box>
             <p>media@renci.org</p>
             <p>919-445-9640</p>
           </Grid>
-          <Grid item xs={ 12 } md={ 4 } lg={ 4 } className="link-group">
+          <Grid item xs={ 12 } sm={ 4 } md={ 3 } lg={ 3 } className="link-group">
+            <Box component="span" sx={{
+              display: 'block',
+              minHeight: '60px',
+              minWidth: '120px',
+              backgroundImage: `url(${ renciLogo })`,
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: '0% 50%',
+            }} />
+            <br/>
             <strong>Connect</strong>
             <ul className="link-list">
               <li><Link to="https://www.twitter.com/RENCI">Twitter</Link></li>

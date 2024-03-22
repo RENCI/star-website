1829a2bbfbb9b31bc46af9bfc0a93d26510232d3
Matt Watson <mwatson@renci.org>
2024-03-22 12:30:04 -0400

    fallback to sample data on fetch error
    fix debug token

--- plugins/gatsby-source-monday/gatsby-node.js
+++ plugins/gatsby-source-monday/gatsby-node.js
@@ -4,9 +4,39 @@ const fetchBoardData = require('./fetch-board-data')
 const assemblePositionData = require('./assemble-positions')
 const assembleColumnData = require('./assemble-columns')
 
+const sampleData = require('./sample-data.json')
+
 const axiosOptions = { headers: {
   'API-Version': '2023-10',
 } }
+
+const createPositionNodes = (gatsbyApi, positionData) => {
+  for (const position of positionData) {
+    gatsbyApi.actions.createNode({
+      ...position,
+      parent: null,
+      children: [],
+      internal: {
+        type: 'MondayItem',
+        contentDigest: gatsbyApi.createContentDigest(position)
+      }
+    })
+  }
+}
+
+const createColumnNodes = (gatsbyApi, columnData) => {
+  for (const column of columnData) {
+    gatsbyApi.actions.createNode({
+      ...column,
+      parent: null,
+      children: [],
+      internal: {
+        type: 'MondayColumn',
+        contentDigest: gatsbyApi.createContentDigest(column)
+      }
+    })
+  }
+}
 
 exports.sourceNodes = async (gatsbyApi, pluginOptions) => {
   const { actions, createContentDigest, createNodeId, reporter } = gatsbyApi
@@ -28,17 +58,7 @@ exports.sourceNodes = async (gatsbyApi, pluginOptions) => {
       throw new Error(`Failed to process column data`)
     }
     sourcingTimer.setStatus(`Sourced ${ columnData.length } columns`)
-    for (const column of columnData) {
-      actions.createNode({
-        ...column,
-        parent: null,
-        children: [],
-        internal: {
-          type: 'MondayColumn',
-          contentDigest: createContentDigest(column)
-        }
-      })
-    }
+    createColumnNodes(gatsbyApi, columnData)
     sourcingTimer.setStatus(`Created ${ columnData.length } column nodes`)
     
     // massage position data and create position nodes
@@ -48,21 +68,20 @@ exports.sourceNodes = async (gatsbyApi, pluginOptions) => {
       throw new Error(`Failed to process position data`)
     }
     sourcingTimer.setStatus(`Sourced ${ positionData.length } positions`)
-    for (const position of positionData) {
-      actions.createNode({
-        ...position,
-        parent: null,
-        children: [],
-        internal: {
-          type: 'MondayItem',
-          contentDigest: createContentDigest(position)
-        }
-      })
-    }
+    createPositionNodes(gatsbyApi, positionData)
     sourcingTimer.setStatus(`Created ${ positionData.length } position nodes`)
     
   } catch (error) {
     sourcingTimer.panicOnBuild(error)
+    sourcingTimer.setStatus(`Could not source positions from API; using fallback data`)
+
+    sourcingTimer.setStatus(`Assembling sample position data`)
+    createColumnNodes(gatsbyApi, sampleData.columns)
+    
+    sourcingTimer.setStatus(`Assembling sample position data`)
+    createPositionNodes(gatsbyApi, sampleData.positions)
+
+    sourcingTimer.setStatus(`Created ${ sampleData.positions.length } sample position nodes`)
   }
 }
 

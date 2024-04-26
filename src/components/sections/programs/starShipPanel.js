e6870efab3b42888a76d657c5c1943b9fb651de0
suejinkim20 <kim.suejin.20@gmail.com>
2024-04-26 09:46:46 -0400

    add in starship panel content

--- src/components/sections/programs/starShipPanel.js
+++ src/components/sections/programs/starShipPanel.js
@@ -7,11 +7,59 @@ import {
   Box,
   Button
 } from '@mui/joy'
+import { List } from '../../list'
 
 export const StarShipPanel = ({title, content}) => {
   return (
     <Container maxWidth="md" sx={{margin: '1.5rem auto'}}>
       <Typography level="h3" textAlign="center" gutterBottom>{title}</Typography>
+      <Typography>{content.description}</Typography>
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        gap={ 4 }
+        sx={{display: 'flex', marginTop: '1.5rem'}}
+      >
+        <Box flex={1}>
+          <Typography level="h4">What to Expect</Typography>
+          <List>
+            {content.whatToExpect.map((item)=> (
+              <ListItem>{item}</ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box flex={1}>
+          <Typography level="h4">Previous STARships have offered experience in:</Typography>
+          <List>
+            {content.previousExperienceOffered.map((item)=> (
+              <ListItem>{item}</ListItem>
+            ))}
+          </List>
+        </Box>
+      </Stack>
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        gap={ 4 }
+        sx={{display: 'flex', marginTop: '1.5rem'}}
+      >
+        <Box flex={1}>
+          <Typography level="h4">Requirements:</Typography>
+          <List>
+            {content.requirements.map((item)=> (
+              <ListItem>{item}</ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box flex={1}>
+          <Typography level="h4">Student Responsibilities:</Typography>
+          <Stack
+            gap={ 4 }
+            sx={{display: 'flex', marginTop: '1.5rem', padding: '0 2.5rem'}}
+          >
+            <Button>STARship Responsibilities</Button>
+            <Button>Extended Student Responsibilities*</Button>
+          </Stack>
+        </Box>
+      </Stack>
     </Container>
   )
 }

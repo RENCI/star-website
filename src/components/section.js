a888cee77c1774abfa4f6e4f870b0c83ddef49d8
suejinkim20 <kim.suejin.20@gmail.com>
2024-04-26 23:09:58 -0400

    adjust styles for section component

--- src/components/section.js
+++ src/components/section.js
@@ -14,17 +14,16 @@ export const Section = ({
   // these seem like top priority customizations
   // to surface, but set reasonable defaults.
   backgroundColor = 'transparent',
-  height = '75vh',
   // make room for any additional style customization.
   customStyles = {},
-  title
+  title,
+  titleColor
 }) => {
   return (
     <Sheet
       component="section"
       id={ id }
       sx={{
-        height,
         backgroundColor,
         display: 'flex',
         justifyContent: 'stretch',
@@ -37,7 +36,7 @@ export const Section = ({
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          my: 2,
+          py: 8,
           mx: 'auto',
         },
         '.height-indicator': {
@@ -53,14 +52,14 @@ export const Section = ({
         ...customStyles,
       }}
     >
-      <Container className="section-container">
-        {title && <Typography level="h2">{title}</Typography>}
+      <Container className="section-container" maxWidth="md">
+        {title && <Typography level="h2" sx={{color: titleColor ? titleColor : null}}>{title}</Typography>}
         { children }
       </Container>
       {
         process.env.NODE_ENV === 'development' && SHOW_SECTION_HEIGHTS && (
           <span className="height-indicator">
-            height = { JSON.stringify(height) }
+            {/* height = { JSON.stringify(height) } */}
           </span>
         )
       }

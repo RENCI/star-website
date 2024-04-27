c1f3475b485898deb189e19a00806327b5195655
suejinkim20 <kim.suejin.20@gmail.com>
2024-04-26 23:10:25 -0400

    clean up process overview section

--- src/components/sections/process-overview.js
+++ src/components/sections/process-overview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Section } from '../section'
 import { TitleBodyItem } from '../title-body-item'
-import { Typography, Box } from '@mui/joy'
+import { Typography, Box, Container } from '@mui/joy'
 import AccordionGroup from '@mui/joy/AccordionGroup';
 import Accordion from '@mui/joy/Accordion';
 import AccordionDetails from '@mui/joy/AccordionDetails';
@@ -13,24 +13,16 @@ export const ProcessOverview = ({ content }) => {
 
   return (
     <Section
-      backgroundColor="#026579 "
-      height="50vh"
+      backgroundColor="#F6F6F6"
+      title={content.title}
     >
-      <Typography level="h2" sx={{color: '#fff', py: 2}}>{content.title}</Typography>
-      <AccordionGroup size="lg" sx={{ maxWidth: 700 }}>
-        {
-          content.steps.map((item)=>{
-            return (
-            <Accordion
-              key={item.title}
-            >
-              <AccordionSummary sx={{ backgroundColor: '#F6F6F6'}}>{item.title}</AccordionSummary>
-              <AccordionDetails sx={{ backgroundColor: '#F6F6F6'}}>{item.description}</AccordionDetails>
-            </Accordion>
-          )})
-        }
-      </AccordionGroup>
-     
+      <Container maxWidth="md">
+      {
+        content.steps.map((item)=>(<TitleBodyItem item={item}/>))
+      }
+
+      </Container>
+
     </Section>
   )
 }

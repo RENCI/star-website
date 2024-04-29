8aa3c539df75143288a0a0667af7e5055d0de8ae
suejinkim20 <kim.suejin.20@gmail.com>
2024-04-29 12:37:09 -0400

    fix forms style and order of sections

--- src/components/sections/forms.js
+++ src/components/sections/forms.js
@@ -7,10 +7,9 @@ export const Forms = ({ content }) => {
   
   return (
     <Section
-    height="40vh"
-    >
-      <Typography level="h2">{content.title}</Typography>
-
+    backgroundColor="#DFE5E8"
+    title={content.title}
+  >
       <Grid container>
       {content.sections.map((section) => (
         <Grid item key={section.heading} 
@@ -20,9 +19,9 @@ export const Forms = ({ content }) => {
           <Typography level="h3" sx={{color: "inherit", marginBottom: '0.25rem'}}>{section.heading}</Typography>
           <Typography level="body-lg" sx={{color: "inherit", marginBottom: '0.5rem'}}>{section.description}</Typography>
           {section.links && section.links.map((link) => (
-              <Link to={link.url} key={link.title}>
-                <Typography sx={{fontWeight: 600, color: '#F9A302'}}>
-                  {link.title}
+              <Link to={link.url} key={link.name}>
+                <Typography sx={{fontWeight: 600}}>
+                  {link.name}
                 </Typography>
               </Link>
           ))}

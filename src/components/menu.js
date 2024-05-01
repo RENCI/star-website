863396867b3d83a53ec974cf5e81122683622fea
suejinkim20 <kim.suejin.20@gmail.com>
2024-05-01 01:05:07 -0400

    fix styles on desktop menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import { Stack } from '@mui/joy'
+import { Stack, Box } from '@mui/joy'
 import { Link } from './link'
+import starLogo from '../images/star-logo-color.png'
 
 export const Menu = () => {
   const data = useStaticQuery(graphql`
@@ -44,7 +45,6 @@ export const Menu = () => {
             justifyContent: 'center',
             alignItems: 'center',
             borderRadius: 0,
-
             '&[aria-current="page"]': {
               borderBottom: '5px solid #04758e'
             },
@@ -59,9 +59,54 @@ export const Menu = () => {
           fontSize: '120%',
           
         },
-        
+        '.brand': {
+          px: 1,
+          alignSelf:'stretch',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          'a': {
+            px: 4,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 0,
+            '&[aria-current="page"]': {
+              borderBottom: '5px solid #04758e'
+            },
+            '&:hover': {
+              backgroundColor: '#04758e10'
+            },
+          },
+        },
+
       }}
     >
+      <li
+            className="nav-list"
+          >
+      <Link to="/" className="brand">
+          <Box component="span" sx={{
+            display: 'block',
+            minHeight: '40px',
+            minWidth: '60px',
+            backgroundImage: `url(${ starLogo })`,
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: '0% 50%',
+            marginBottom: '0.5rem',
+            'a': {
+              '&[aria-current="page"]': {
+                borderBottom: '5px solid #04758e'
+              },
+              '&:hover': {
+                backgroundColor: '#04758e10'
+              },
+            },
+          }} />
+        </Link>
+
+          </li>
       {
         data.themeYaml.navigation.map(({ label, path }) => (
           <li

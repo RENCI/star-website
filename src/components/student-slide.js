3a7d0848949d90d6e2d754bcc7d99400394315d5
suejinkim20 <kim.suejin.20@gmail.com>
2024-08-14 16:57:11 -0400

    combine responsive styles into one StudentSlide

--- src/components/student-slide.js
+++ src/components/student-slide.js
@@ -15,9 +15,11 @@ export const StudentSlide = ({student}) => {
   return (
     <Card 
       sx={{
-        height: '220px',
-        marginTop: '0.5rem',
-        padding: '1rem 2.5rem 3rem',
+        height: {xs: '180px', sm: '200px', md: '220px'},
+        px: {xs: '1rem', sm: '2rem', md: '2.5'},
+        pt: '1rem', pb: '3rem',
+        mt: '0.5rem',
+        mx: '1rem', 
         backgroundColor: '#ECF1F2',
       }}
     >
@@ -27,7 +29,10 @@ export const StudentSlide = ({student}) => {
         justifyContent: 'center'
       }}>
         <Typography level="h3" align="center"
-          sx={{ fontWeight: 600 }}
+          sx={{
+            fontSize: {xs: '1.2rem', sm: '1.25rem', md: '1.6rem'},
+            fontWeight: 600
+          }}
         >
           {project_description}
         </Typography>
@@ -40,7 +45,7 @@ export const StudentSlide = ({student}) => {
         </Typography>
         <Typography align="center"
           sx={{
-            fontSize: '1rem',
+            fontSize: {xs: '0.9rem', sm: '1rem'},
           }}
         >
           {title}, {semester}
@@ -59,62 +64,3 @@ export const StudentSlide = ({student}) => {
 }
 
 
-export const MobileSlide = ({student}) => {
-  const { 
-    student_name, 
-    semester,
-    title, 
-    project_description,
-    project_link_text, 
-    project_link } = student
-
-  return (
-    <Card 
-      sx={{
-        height: {xs: '180px', sm: '200px'},
-        px: {xs: '1rem', sm: '2rem'},
-        pb: '3rem',
-        mx: '1rem', 
-        backgroundColor: '#ECF1F2',
-      }}
-    >
-      <CardContent sx={{
-        display: 'flex', 
-        flexDirection: 'column', 
-        justifyContent: 'center'
-      }}>
-      <Typography level="h3" align="center"
-        sx={{
-          fontSize: {xs: '1.2rem', sm: '1.25rem'},
-          fontWeight: 600
-        }}
-      >
-          {project_description}
-        </Typography>
-        <Typography level="h4" align="center"
-          sx={{
-            fontSize: '1rem',
-          }}
-        >
-          {student_name}
-        </Typography>
-        <Typography align="center"
-          sx={{
-            fontSize: '0.9rem',
-            fontStyle: 'italic',
-          }}
-        >
-          {title}, {semester}
-        </Typography>
-      </CardContent>
-      <CardActions sx={{margin: '0 auto'}}>
-        <Button
-          component={Link}
-          to={project_link}
-          noIcon
-          external
-          >{project_link_text}</Button>
-      </CardActions>
-    </Card>
-  )
-}

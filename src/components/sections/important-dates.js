0419b13ce7b0535dcf0750fd9efe61d3945c1542
suejinkim20 <kim.suejin.20@gmail.com>
2024-08-14 16:52:28 -0400

    remove dates variables from monday plugin

--- src/components/sections/important-dates.js
+++ src/components/sections/important-dates.js
@@ -3,16 +3,7 @@ import { Section } from "../section"
 import { DatesTable } from '../dates-table'
 import ImportantDatesContent from '../../content/sections/important-dates.yaml'
 
-export const ImportantDates = ({ content }) => {
-  const rows = content.nodes.map(({ name, dates, audience }) => ({
-    name,
-    dates: dates.reduce((acc, d) => {
-      acc[d.year] = d.date
-      return acc
-    }, {}),
-    audience,
-  }))
-
+export const ImportantDates = ( ) => {
 
   return (
     <Section title='Important Dates' backgroundColor="#fff" height="55vh">
